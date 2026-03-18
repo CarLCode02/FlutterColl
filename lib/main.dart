fcdf2494d90296d45f8b8ae9f06df2a9826c2b0c
Ike Renson Salcedo Landong <iklandong@my.cspc.edu.ph>
2026-03-18 09:02:23 +0800

    my changes

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'BICOL REGION GENERAL HOSPITAL AND GERIATRIC MEDICAL Cener',
+      title: 'BICOL REGION GENERAL HOSPITAL AND GERIATRIC MEDICAL Cntaur',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const HomePage(),
     );
